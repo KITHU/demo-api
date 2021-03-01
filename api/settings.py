10d01de25b8b69fd45200e436b7d566dffc2fe47
KITHU <james.kithu2@gmail.com>
2021-03-01 19:01:10 +0300

    - add procfile - and badges

--- api/settings.py
+++ api/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = getenv('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 AUTH_USER_MODEL = 'authentication.User'
 # Application definition
